6aea0d3b42c605d48b49ce60ed01892a7f36b1b7
Dereck Piché <pichedereck@gmail.com>
2023-08-31 08:36:17 -0400

    .

--- src/dense.jl
+++ src/dense.jl
@@ -1,8 +1,9 @@
-mutable struct dense
+struct dense{} <: Mutator
     w::Matrix
     b::Vector
     f::Function
 end
+
 
 function dense(inDim::Int, outDim::Int, f::Function)
     # returns dense composite according to dimensions
@@ -11,11 +12,12 @@ function dense(inDim::Int, outDim::Int, f::Function)
     return dense(w, b, f)
 end
 
-function (d::dense)(v::AbstractVector)
+function (d::dense)(v::AbstractArray{<:Real, 1})
     return d.f( *(d.w, v) + d.b )
 end
 
-function (d::dense)(x::Array{<:Number, 2})
+function (d::dense)(x::Array{<:Real, 2})
+    # array cells are subtypes of Real, not Real
     # apply dense to each row
     y = Array{Number, 2}(undef, size(x)[1], size(d.w)[1])
     for (rowInd, v) in enumerate(eachrow(x))
--- src/train.jl
+++ src/train.jl
@@ -13,7 +13,7 @@ function train(
     for _ in 1:epochs
         for i in 1:iterations
             println(lossF(model(x[i, :]), y[i, :]))
-            model = descent(x[i, :], y[i, :], lossF, model, stepSize)
+            model = descentStep(x[i, :], y[i, :], lossF, model, stepSize)
         end
     end
 end
