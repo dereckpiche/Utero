5e37c21a0d27d36bdbb4626cbd52078e7cc0ae85
Dereck Piché <80223269+ScierKnave@users.noreply.github.com>
2024-05-19 20:12:06 -0400

    .

--- src_cpp/auto_diff/matmul.cpp
+++ src_cpp/auto_diff/matmul.cpp
@@ -6,3 +6,4 @@ struct tensor matmul(struct tensor x,
     // TODO;
     return;
 };
+
--- src_cpp/auto_diff/tensor.cpp
+++ src_cpp/auto_diff/tensor.cpp
@@ -1,7 +1,34 @@
+#include <vector> 
+
+typedef struct vect {
+    float* emts; // pointer to first element in the memory
+    int size; // total number of elements
+}
 
 typedef struct tensor {
-
+    float* values;
+    std::vect<int>& shape;
+    std::vect<int>& stride;
 } tensor;
 
 
+tensor talloc(const vect<int>& shape) {
+    float* values = malloc(shape.size());
+    for (int i = 0; i < shape.size(); i++) {
+        
+    }
+};
+
+
+int get_index(tensor t, const vect<int>& indices){
+    if (indices.size() != t.shape.size()) {
+        printf("Number of indices does not match.");
+        exit(1)
+    };
+    int index = 0;
+    for (int i = 0; i < indices.size(); i++){
+        index+=indices[i]*t.strides[i];
+    }
+    return index;
+}
 
