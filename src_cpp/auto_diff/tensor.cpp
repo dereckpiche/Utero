f63de878f6cd925b946ba00f71098d16d2c88e7b
Dereck Piché <pichedereck@gmail.com>
2024-05-22 20:05:36 -0400

    working matrix print

--- src_cpp/auto_diff/tensor.cpp
+++ src_cpp/auto_diff/tensor.cpp
@@ -1,30 +1,32 @@
 #include <vector> 
+#include <cstdlib>
+#include <iostream>
 
 typedef struct tensor {
-    float& values; // reference to start of contiguous memory string
+    float* values; // reference to start of contiguous memory string
     int size;
-    std::vect<int>& shape;
-    std::vect<int>& stride;
+    std::vector<int> shape;
+    std::vector<int> strides;
 } tensor;
 
-tensor talloc(vect<int>& shape) { // tensor allocation
-    std::vect<int> strides(shape.size(), 1); 
-    int stride = 1;
+tensor talloc(std::vector<int> shape) { // tensor allocation
+    std::cout << shape.size();
+    std::vector<int> strides(shape.size(), 1); 
     // precompute strides for faster access
     // going in reverse because more consecutive accesses for rightmost indices
-    for (int i = shape.size()-2; i <= 0; i--) {
-        strides[i] = shape[i-1] * strides[i-1]; 
+    for (int i = shape.size()-2; i >= 0; i--) {
+        strides[i] = shape[i+1] * strides[i+1]; 
     }
-    int size = stride * shape[0];
-    float* values = calloc(size);
-    tensor t = {values, size, shape, &strides};
-};
+    int size = strides[0] * shape[0];
+    float* values = (float*)std::calloc(size, sizeof(float));
+    tensor t = {values, size, shape, strides};
+    return t;
+}
 
-
-int get_index(tensor t, vect<int>& indices){
+int get_index(tensor t, std::vector<int>& indices){
     if (indices.size() != t.shape.size()) {
         printf("Number of indices does not match tensor shape.");
-        exit(1)
+        exit(1);
     };
     int index = 0;
     for (int i = 0; i < indices.size(); i++){
@@ -33,11 +35,32 @@ int get_index(tensor t, vect<int>& indices){
     return index;
 }
 
-float access(tensor t, vect<int>& indices){
+float access(tensor t, std::vector<int> indices){
     return t.values[get_index(t, indices)];
 }
 
-
-int main() {
-    
+void set(tensor t, float value, std::vector<int> indices){
+    // TODO;
 }
+
+void print(tensor t){
+    std::cout << t.shape.size();
+    if (t.shape.size() == 2) {
+        std::cout << "yo";
+        std::string matrix = "\n";
+        for (int i=0; i< t.shape[0]; i++){
+            for (int j=0; j<t.shape[1];j++){
+                matrix += std::to_string(access(t, {i,j})) + " ";
+            }
+            matrix = matrix + "\n";
+        }
+        std::cout << matrix;
+    }
+}
+ 
+int main() {
+    tensor t = talloc({7,4});
+    print(t);
+    return 0;
+}
+
